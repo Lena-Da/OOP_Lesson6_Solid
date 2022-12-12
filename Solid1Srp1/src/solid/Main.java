15f946ff203912f2ddb7b846c5b7d3e70062b8f9
Lena <len.dal@yandex.ru>
2022-12-12 15:42:20 +0300

    class work

--- Solid1Srp1/src/solid/Main.java
+++ Solid1Srp1/src/solid/Main.java
@@ -1,12 +1,14 @@
 package solid;
 
+import solid.srp.FigureManager;
 import solid.srp.Point;
 import solid.srp.Square;
 
 public class Main {
     public static void main(String[] args) {
+        FigureManager figureManager = new FigureManager(2);
         Square square = new Square(new Point(1,1), 5);
         System.out.printf("Площадь фигуры: %d \n", square.getArea());
-        square.draw();
+        figureManager.draw(square);
     }
 }
